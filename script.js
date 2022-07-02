98e60572e38cad50334487fa037d20d7ef35eee1
Steve <sgeiger5516@gmail.com>
2022-07-02 15:20:17 -0400

    Changed grid generation to function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-for (let i=0; i < 256; i++){
-    let box = document.createElement('div');
-    box.className = "square";
-    let space = document.createElement('p');
-    space.innerText = " ";
-    box.appendChild(space);
-    document.getElementById('gridcontainer').appendChild(box);
+function populateBoard(size) {
+    let board = document.querySelector("#gridcontainer");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i=0; i < 256; i++){
+        let box = document.createElement('div');
+        box.className = "square";
+        box.style.backgroundColor = "blue";
+        board.insertAdjacentElement("beforeend", box);
+    }
 }
+
+populateBoard(16);
