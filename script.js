ad970006a3de26ed3247b62f72c3a5b84ef82f4b
Steve <sgeiger5516@gmail.com>
2022-07-02 15:43:44 -0400

    Added mouseover color change

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+
+
+
 function populateBoard(size) {
     let board = document.querySelector("#gridcontainer");
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i=0; i < 256; i++){
+    for (let i=0; i < size ** 2; i++){
         let box = document.createElement('div');
         box.className = "square";
         box.style.backgroundColor = "blue";
+        box.addEventListener("mouseenter", function(){
+            box.style.backgroundColor = "red";
+        });
         board.insertAdjacentElement("beforeend", box);
     }
 }
 
-populateBoard(16);
+populateBoard(32);
+
+
